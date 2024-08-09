cc7859aaa145e859d7d8c45e50588eb13d3ef46a
Junaid Younus <soomroj24@gmail.com>
2024-08-09 11:23:38 +0500

    Fetching Cars from Apex to carListItem

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars';
 
-export default class CarTileList extends LightningElement {}
+export default class CarTileList extends LightningElement {
+    @wire(getCars)
+    carsHandler({data,error}){
+        if(data){
+            console.log(data);
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+}
